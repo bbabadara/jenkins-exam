815e2f232088c64f6d4ca07c8c9ea752490d56b1
bbabadara <bbabadara@gmail.com>
2025-09-20 11:04:19 +0000

    fourth jenk test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
   public String home() {
-    return "Hello hello world! This web application created uisng java spring boot framework and depolyed in kubernetes .";
+    return "Hello  world! This web application created uisng java spring boot framework and depolyed in kubernetes .";
   }
 }
